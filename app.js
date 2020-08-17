5bcff0af658ed6f6f333e0df59456afa1ba42ddb
oieusouobixo <guicosta87@yahoo.com.br>
2020-08-17 17:45:33 -0300

    added assets, js, html and css

---
+++ app.js
@@ -0,0 +1,206 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    //card options
+    const cardArray = [
+        {
+            name: 'balloons',
+            img: 'assets/img/balloons.png'
+        },
+        {
+            name: 'balloons',
+            img: 'assets/img/balloons.png'
+        },
+        {
+            name: 'cap',
+            img: 'assets/img/cap.png'
+        },
+        {
+            name: 'cap',
+            img: 'assets/img/cap.png'
+        },
+        {
+            name: 'cat',
+            img: 'assets/img/cat.png'
+        },
+        {
+            name: 'cat',
+            img: 'assets/img/cat.png'
+        },
+        {
+            name: 'cupcake',
+            img: 'assets/img/cupcake.png'
+        },
+        {
+            name: 'cupcake',
+            img: 'assets/img/cupcake.png'
+        },
+        {
+            name: 'doggo',
+            img: 'assets/img/doggo.png'
+        },
+        {
+            name: 'doggo',
+            img: 'assets/img/doggo.png'
+        },
+        {
+            name: 'elephant',
+            img: 'assets/img/elephant.png'
+        },
+        {
+            name: 'elephant',
+            img: 'assets/img/elephant.png'
+        },
+        {
+            name: 'flower',
+            img: 'assets/img/flower.png'
+        },
+        {
+            name: 'flower',
+            img: 'assets/img/flower.png'
+        },
+        {
+            name: 'giraffe',
+            img: 'assets/img/giraffe.png'
+        },
+        {
+            name: 'giraffe',
+            img: 'assets/img/giraffe.png'
+        },
+        {
+            name: 'icecream',
+            img: 'assets/img/icecream.png'
+        },
+        {
+            name: 'icecream',
+            img: 'assets/img/icecream.png'
+        },
+        {
+            name: 'jar',
+            img: 'assets/img/jar.png'
+        },
+        {
+            name: 'jar',
+            img: 'assets/img/jar.png'
+        },
+        {
+            name: 'kangaroo',
+            img: 'assets/img/kangaroo.png'
+        },
+        {
+            name: 'kangaroo',
+            img: 'assets/img/kangaroo.png'
+        },
+        {
+            name: 'leaf',
+            img: 'assets/img/leaf.png'
+        },
+        {
+            name: 'leaf',
+            img: 'assets/img/leaf.png'
+        },
+        {
+            name: 'monkey',
+            img: 'assets/img/monkey.png'
+        },
+        {
+            name: 'monkey',
+            img: 'assets/img/monkey.png'
+        },
+        {
+            name: 'owl',
+            img: 'assets/img/owl.png'
+        },
+        {
+            name: 'owl',
+            img: 'assets/img/owl.png'
+        },
+        {
+            name: 'parfum',
+            img: 'assets/img/parfum.png'
+        },
+        {
+            name: 'parfum',
+            img: 'assets/img/parfum.png'
+        },
+        {
+            name: 'plane',
+            img: 'assets/img/plane.png'
+        },
+        {
+            name: 'plane',
+            img: 'assets/img/plane.png'
+        },
+        {
+            name: 'queen',
+            img: 'assets/img/queen.png'
+        },
+        {
+            name: 'queen',
+            img: 'assets/img/queen.png'
+        },
+        {
+            name: 'ring',
+            img: 'assets/img/ring.png'
+        },
+        {
+            name: 'ring',
+            img: 'assets/img/ring.png'
+        },
+        {
+            name: 'snowman',
+            img: 'assets/img/snowman.png'
+        },
+        {
+            name: 'snowman',
+            img: 'assets/img/snowman.png'
+        },
+        {
+            name: 'turtle',
+            img: 'assets/img/turtle.png'
+        },
+        {
+            name: 'turtle',
+            img: 'assets/img/turtle.png'
+        },
+        {
+            name: 'umbrella',
+            img: 'assets/img/umbrella.png'
+        },
+        {
+            name: 'umbrella',
+            img: 'assets/img/umbrella.png'
+        },
+        {
+            name: 'violin',
+            img: 'assets/img/violin.png'
+        },
+        {
+            name: 'violin',
+            img: 'assets/img/violin.png'
+        },
+        {
+            name: 'xilo',
+            img: 'assets/img/xilo.png'
+        },
+        {
+            name: 'xilo',
+            img: 'assets/img/xilo.png'
+        },
+        {
+            name: 'yatch',
+            img: 'assets/img/yatch.png'
+        },
+        {
+            name: 'yatch',
+            img: 'assets/img/yatch.png'
+        },
+        {
+            name: 'zebra',
+            img: 'assets/img/zebra.png'
+        },
+        {
+            name: 'zebra',
+            img: 'assets/img/zebra.png'
+        },
+    ]
+})
